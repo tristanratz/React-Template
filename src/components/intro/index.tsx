17db9d414fcc4a88ffb249364e2330e22dc6af44
Tristan Ratz <triratz@aol.com>
2020-04-07 16:28:24 +0200

    Add basic structure

---
+++ src/components/intro/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export class Intro extends React.Component {
+
+    render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
+        return <h1>Tristan Ratz</h1>;
+    }
+}
---
+++ src/components/projects/project.tsx
@@ -0,0 +1,5 @@
+import React from  'react';
+
+export class Project extends React.Component {
+
+}
