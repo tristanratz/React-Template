31fc6034e03b48e8e731dd044d90342d8a0b76b9
Tristan Ratz <triratz@aol.com>
2020-04-27 22:06:05 +0200

    Update texts

---
+++ src/store/person.tsx
@@ -0,0 +1,15 @@
+
+interface Person {
+    firstName: string,
+    lastName: string,
+    email: string,
+    website: string,
+}
+
+export const person: Person = {
+    firstName: "Tristan",
+    lastName: "Ratz",
+    email: "mail@tristanratz.com",
+    website: "tristanratz.com",
+
+}
